cf05f1f144f15f04766027e2492e728021a1f7c2
Jeffreychen99 <personjeffrey@gmail.com>
2018-07-13 10:17:06 -0400

    Initial Commit

---
+++ Variant Perception/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Variant Perception
+//
+//  Created by Jeffrey Chen on 7/13/18.
+//  Copyright © 2018 Jeffrey Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
